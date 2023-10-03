5394c7f4a1fc84ebca23a2375e84498054ee66e3
Hardik <hardikjhaveri952@gmail.com>
2023-10-03 23:15:14 +0530

    New Activity Added

--- Module 3/AndroidAssignment/app/src/main/java/com/example/androidassignment/MyActivity2.kt
+++ Module 3/AndroidAssignment/app/src/main/java/com/example/androidassignment/MyActivity2.kt
@@ -3,9 +3,9 @@ package com.example.androidassignment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class MyActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_my2)
     }
 }
